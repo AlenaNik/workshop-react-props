2f7c7585c11c41f362a85748796be2ea78c0a566
AlenaNik <alena.nik.digital@gmail.com>
2019-04-22 17:59:07 +0200

    brach with button is working

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import User from './User';
+import Counter from "./Counter";
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            Simpsons Avatars
-          </header>
-          <User name="Bart Simpson" avatar="https://www.drupal.org/files/issues/default-avatar.png"/>
-          <User name="Homer Simpson" avatar="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png"/>
-          <User name="Marge Simpson" avatar="https://static.simpsonswiki.com/images/0/0b/Marge_Simpson.png"/>
-          <User name="Liza simpson" star />
+        <div>
+           <Counter />
+           <Counter />
         </div>
     );
   }
